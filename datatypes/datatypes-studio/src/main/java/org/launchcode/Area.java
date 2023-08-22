0c942df3ad616fa4b204d4927274734997e9327a
Melany Willis <melexiw@gmail.com>
2023-08-22 14:33:30 -0500

    Tried to add logic

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,5 +1,6 @@
 package org.launchcode;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Area {
@@ -7,9 +8,22 @@ public class Area {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the circle radius: ");
-
+        boolean isValidNum = false;
         double radius = input.nextDouble();
-        System.out.println("The area of the circle is " + Circle.getArea(radius));
 
+        try {
+            while (!isValidNum)
+                if (radius < 0) {
+                    System.out.println("Please enter a positive number");
+                    radius = input.nextDouble();
+                } else {
+                    isValidNum = true;
+                    System.out.println("The area of the circle is " + Circle.getArea(radius));
+                }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Not valid input! Quitting.");
+        }
+        input.close();
     }
 }
