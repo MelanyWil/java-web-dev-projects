6d50cf7b9772bc151c9f911f8fcfe0eedc3fe97f
Melany Willis <melexiw@gmail.com>
2023-09-27 13:35:39 -0500

    Sorted Flavor ArrayList by name.

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,14 +1,20 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        flavors.sort(comparator);
+        for(Flavor flavor: flavors) {
+            System.out.println(flavor.getName());
+        }
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
